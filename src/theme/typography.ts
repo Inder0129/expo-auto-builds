05afe231a0d1f1457fd2e9c71658529aa3a126e4
Inder0129 <159561982+Inder0129@users.noreply.github.com>
2026-04-16 10:16:01 +0530

    Clean repo for new project

--- src/theme/typography.ts
+++
@@ -1,9 +0,0 @@
-import { moderateScaleFactor } from './index';
-export const fontFamily = 'Inter';
-export const fontSizes = {
-  xs: 12 * moderateScaleFactor,
-  sm: 14 * moderateScaleFactor,
-  md: 16 * moderateScaleFactor,
-  lg: 20 * moderateScaleFactor,
-  xl: 24 * moderateScaleFactor
-};
