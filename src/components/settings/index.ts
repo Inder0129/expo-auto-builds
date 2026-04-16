4f9ecbd0ad6841b2101d3c9f15941901a58cbd65
Inder0129 <159561982+Inder0129@users.noreply.github.com>
2026-04-16 12:59:28 +0530

    Clean repo for new project

--- src/components/settings/index.ts
+++
@@ -1,2 +0,0 @@
-export { ThemeSelector } from './theme-selector';
-export { SettingsList } from './settings-list';
--- src/constants/routes.ts
+++
@@ -1,7 +0,0 @@
-export const ROUTES = {
-  TABS: {
-    CALCULATOR: '/(tabs)/calculator',
-    HISTORY: '/(tabs)/history',
-    SETTINGS: '/(tabs)/settings'
-  }
-} as const;
